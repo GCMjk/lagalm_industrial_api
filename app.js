2fe3a3176c1ef9c7797bab3d8f43a99ce1e45256
Martin Lagalm <majok5040@gmail.com>
2023-01-25 11:49:15 -0600

    base proyect

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const chalk = require('chalk');
+
+const port = process.env.PORT || 4201;
+
+const app = express();
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
+app.listen(port, () => {
+    console.log('Serve at', chalk.green(`http://localhost:${port}`));
+});
+
+const config = {
+    limit: '50mb',
+    extended: true
+}
+
+app.use(bodyparser.urlencoded(config));
+app.use(bodyparser.json(config));
+
+// CORS
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*'); 
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
+    next();
+});
+
+module.exports = app;
